5b01c4b93d0fbe02c7a8169816fd64b04132d304
EC2 Default User <ec2-user@ip-172-31-42-110.ec2.internal>
2021-06-04 13:29:04 +0000

    [ADD]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-   protect_from_forgery
+  protect_from_forgery :except => [:create]
 
   def top
   end
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book)
   end
 
 
@@ -31,6 +31,13 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+
   end
 
   private
